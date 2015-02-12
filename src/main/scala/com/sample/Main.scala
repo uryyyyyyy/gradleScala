0a7b5b8146f06fac30a5251595d9349b989669cc
shiba <koki305@gmail.com>
2015-02-12 22:47:10 +0900

    init

---
+++ src/main/scala/com/sample/Main.scala
@@ -0,0 +1,7 @@
+package com.sample
+
+object Main {
+	def main(args: Array[String]) :Unit = {
+		println("Hello World")
+	}
+}
---
+++ src/test/scala/com/sample/MainTest.scala
@@ -0,0 +1,12 @@
+package com.sample
+
+import org.junit.Assert._
+import org.junit.Test
+
+class MainTest {
+
+	@Test
+	def now {
+		assertEquals("ss", "ss")
+	}
+}
